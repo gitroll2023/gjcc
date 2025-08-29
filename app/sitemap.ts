edaa34210cc8e31bf4a3237446ca3db91f7595f9
gitroll2023 <class2023.05@gmail.com>
2025-08-29 15:13:59 +0900

    a

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from 'next'
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = 'https://gjcc.kr'
+  const baseUrl = 'https://gjcc.or.kr'
   
   return [
     {
